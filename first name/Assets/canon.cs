a2b42b75d4fcb4665b4f2c9ee0d6d14743467fb9
SPACE <weber0216@gmail.com>
2020-08-13 17:19:31 +0800

    complete warning system

--- first name/Assets/canon.cs
+++ first name/Assets/canon.cs
@@ -1,13 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using UnityEngine;
 
 public class canon : MonoBehaviour
 {
-
+    float coorXUp;
+    float coorXDown;
+    float coorYLeft;
+    float coorYRight;
     public GameObject bulletHorizontal;
     public GameObject bulletVertical;
-    public Transform bulletSpawn;
+    public GameObject warningVertical;
+    public GameObject warningHorizontal;
     public fullScreenTouch fullscreentouch;
     public gameUI gameui;
 
@@ -26,15 +31,18 @@ public class canon : MonoBehaviour
     // bullet fire form left
     void fireLeft()
     {
-        // Create the Bullet from the Bullet Prefab
-        /*GameObject bullet = (GameObject)Instantiate(
-            bulletPrefab,
-            bulletSpawn.position,
-            bulletSpawn.rotation);*/
-
         float coorY = Random.Range(4.0f, -4.2f);
-    
-        GameObject bullet = (GameObject)Instantiate(
+        coorYLeft = coorY;
+
+        GameObject warning = (GameObject)Instantiate(
+           warningHorizontal,
+           new Vector3(3.37f, coorY, 0), new Quaternion(0, 0, 180, 0));
+
+        Destroy(warning, 0.8f);
+
+        StartCoroutine(wait(3));
+
+        /*GameObject bullet = (GameObject)Instantiate(
             bulletHorizontal,
             new Vector3(5, coorY, 0), new Quaternion(0, 0, 180, 0));
 
@@ -42,15 +50,24 @@ public class canon : MonoBehaviour
         bullet.GetComponent<Rigidbody2D>().velocity = bullet.transform.right *6;
 
         // Destroy the bullet after 2 seconds
-        Destroy(bullet, 2f);
+        Destroy(bullet, 2f);*/
     }
 
     // bullet fire form right
     void fireRight()
     {
         float coorY = Random.Range(4.0f, -4.2f);
+        coorYRight = coorY;
 
-        GameObject bullet = (GameObject)Instantiate(
+        GameObject warning = (GameObject)Instantiate(
+           warningHorizontal,
+           new Vector3(-2.25f, coorY, 0), new Quaternion(0, 0, 0, 0));
+
+        Destroy(warning, 0.8f);
+
+        StartCoroutine(wait(1));
+
+        /*GameObject bullet = (GameObject)Instantiate(
             bulletHorizontal,
             new Vector3(-3.5f, coorY, 0), new Quaternion(0, 0, 0, 0));
 
@@ -58,31 +75,51 @@ public class canon : MonoBehaviour
         bullet.GetComponent<Rigidbody2D>().velocity = bullet.transform.right * 6;
 
         // Destroy the bullet after 2 seconds
-        Destroy(bullet, 2f);
+        Destroy(bullet, 2f);*/
     }
 
     // bullet fire form above
     void fireUp()
     {
         float coorX = Random.Range(3.2f, -2.1f);
+        coorXUp = coorX;
 
-        GameObject bullet = (GameObject)Instantiate(
-            bulletVertical,
-            new Vector3(coorX, -4.0f, 0), new Quaternion(0, 0, 0, 0));
+        GameObject warning = (GameObject)Instantiate(
+           warningVertical,
+           new Vector3(coorX, -4.353f, 0), new Quaternion(0, 0, 0, 0));
 
-        // Add velocity to the bullet
-        bullet.GetComponent<Rigidbody2D>().velocity = bullet.transform.up * 6;
+        Destroy(warning, 0.8f);
+   
+        StartCoroutine(wait(0));
 
-        // Destroy the bullet after 2 seconds
-        Destroy(bullet, 1.3f);
+        
+           /* GameObject bullet = (GameObject)Instantiate(
+                bulletVertical,
+                new Vector3(coorX, -4.0f, 0), new Quaternion(0, 0, 0, 0));
+
+            // Add velocity to the bullet
+            bullet.GetComponent<Rigidbody2D>().velocity = bullet.transform.up * 6;
+
+            // Destroy the bullet after 2 seconds
+            Destroy(bullet, 1.3f);*/
+        
     }
 
     // bullet fire form below
     void fireDown()
     {
         float coorX = Random.Range(3.2f, -2.1f);
+        coorXDown = coorX;
 
-        GameObject bullet = (GameObject)Instantiate(
+        GameObject warning = (GameObject)Instantiate(
+           warningVertical,
+           new Vector3(coorX, 3.92f, 0), new Quaternion(0, 0, 180, 0));
+
+        Destroy(warning, 0.8f);
+
+        StartCoroutine(wait(2));
+
+        /*GameObject bullet = (GameObject)Instantiate(
             bulletVertical,
             new Vector3(coorX, 3.5f, 0), new Quaternion(0, 0, 180, 0));
 
@@ -90,7 +127,7 @@ public class canon : MonoBehaviour
         bullet.GetComponent<Rigidbody2D>().velocity = bullet.transform.up * 6;
 
         // Destroy the bullet after 2 seconds
-        Destroy(bullet, 1.3f);
+        Destroy(bullet, 1.3f);*/
     }
 
     void traceFire()
@@ -139,6 +176,64 @@ public class canon : MonoBehaviour
         {
             print("destroy player");
             Destroy(collideObject);
+        }
+    }
+
+    IEnumerator wait(int mode)
+    {
+        yield return new WaitForSeconds(1);
+        createObject(mode);
+    }
+
+    void createObject(int objPos)
+    {
+
+        switch (objPos)
+        {
+            case 0:// up
+                GameObject bulletUp = (GameObject)Instantiate(
+                bulletVertical,
+                new Vector3(coorXUp, -4.0f, 0), new Quaternion(0, 0, 0, 0));
+
+                // Add velocity to the bullet
+                bulletUp.GetComponent<Rigidbody2D>().velocity = bulletUp.transform.up * 6;
+
+                // Destroy the bullet after 2 seconds
+                Destroy(bulletUp, 1.3f);
+                break;
+            case 1:// right
+                GameObject bulletRight = (GameObject)Instantiate(
+                bulletHorizontal,
+                new Vector3(-3.5f, coorYRight, 0), new Quaternion(0, 0, 0, 0));
+
+                // Add velocity to the bullet
+                bulletRight.GetComponent<Rigidbody2D>().velocity = bulletRight.transform.right * 6;
+
+                // Destroy the bullet after 2 seconds
+                Destroy(bulletRight, 2f);
+                break;
+            case 2:// down
+                GameObject bulletDown = (GameObject)Instantiate(
+                bulletVertical,
+                new Vector3(coorXDown, 3.5f, 0), new Quaternion(0, 0, 180, 0));
+
+                // Add velocity to the bullet
+                bulletDown.GetComponent<Rigidbody2D>().velocity = bulletDown.transform.up * 6;
+
+                // Destroy the bullet after 2 seconds
+                Destroy(bulletDown, 1.3f);
+                break;
+            case 3:// left
+                GameObject bulletLeft = (GameObject)Instantiate(
+                bulletHorizontal,
+                new Vector3(5, coorYLeft, 0), new Quaternion(0, 0, 180, 0));
+
+                // Add velocity to the bullet
+                bulletLeft.GetComponent<Rigidbody2D>().velocity = bulletLeft.transform.right * 6;
+
+                // Destroy the bullet after 2 seconds
+                Destroy(bulletLeft, 2f);
+                break;
         }
     }
 }
